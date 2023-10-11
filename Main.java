8dc288c61036d8f3e9b1032ab2df5d1f0430bef1
drewburton <drew.burton.gc@gmail.com>
2023-10-11 10:43:17 -0400

    added test code

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
 	public static void main(String... args) {
-		System.out.println("Hello world!");
+		// fix the following code to print the pattern
+		/*
+			*
+			**
+			***
+			****
+		*/
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 }
